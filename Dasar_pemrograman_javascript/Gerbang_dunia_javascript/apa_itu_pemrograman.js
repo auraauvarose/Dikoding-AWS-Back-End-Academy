71b6d4a11db9a4ae29abf949f017f4861430774f
? <110692056+auraauvarose@users.noreply.github.com>
2025-06-10 19:48:32 +0700

    update

---
+++ Dasar_pemrograman_javascript/Gerbang_dunia_javascript/apa_itu_pemrograman.js
@@ -0,0 +1,4 @@
+/* Apa Itu Pemrograman
+Salah satu bahasa pemrograman untuk komputer adalah JavaScript. Apakah Anda tahu apa itu pemrograman atau bahasa pemrograman? Sebelum melangkah lebih lanjut ke JavaScript, kita perlu tahu makna dari istilah tersebut.
+
+Pemrograman adalah proses menyusun instruksi yang sistematis dan akan dieksekusi oleh komputer. Dari pengertian tersebut, kita dapat memiliki gambaran bahwa ini mirip dengan instruksi kepada suatu objek seperti manusia dalam mengikuti panduan memasak, tetapi dalam dunia pemrograman, komputer adalah objeknya. Manusia membuat beberapa instruksi yang harus dieksekusi oleh komputer dalam bahasa yang dimengertinya, yakni bahasa pemrograman. Menarik, bukan? */
